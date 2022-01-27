f96cdbfdfef97b31a778f9b7649457943885100d
JooKissa <s2000958@edu.bc.fi>
2022-01-27 12:34:04 +0200

    lol ok

---
+++ program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GitCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true){
+                Console.WriteLine("Anna 2 numeroa");
+                int num1, num2;
+                num1 = Convert.ToInt32(Console.ReadLine());
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                int summa = num1 + num2;
+                Console.WriteLine(summa);
+            }
+        }
+    }
+}
